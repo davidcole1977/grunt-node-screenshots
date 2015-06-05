2a0328ea693f9ae2a08db621ff87f22257299096
David Cole <hallo@davidcole.info>
2015-06-05 21:29:33 +0100

    Initial EXTREMELY rough investigation & experimentation.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # grunt-node-screenshots
 
 The aim is to put together a simple grunt/node-based boilerplate that will take screenshots of a variety of web pages and run visual diff tests against a baseline in a similar manner to Phantom CSS.
+
+This is an area for experimentation and prototyping.
+
+Following a wee spot of investigation, the plan is:
+
+* YAML file contains pages config details: friendly names & URLs of pages to be captured & ID(s) of page regions to capture
+* grunt task takes screenshots of pages specified in YAML config, using node-webshot
+* 2 instances of grunt task: one to take baseline images (only run when needed) and one to take current screenshots (run each time we do a comparison)
+* Mocha tests do the diffing assertions using node-resemble-js, saves screenshot diffs only for failing assertions
+* The diffing is done inside a custom chai assertion
+* May need to use global grunt config variables (yuck!) to specify image difference tolerance, screenshot locations etc. (http://gruntjs.com/frequently-asked-questions#how-can-i-share-parameters-across-multiple-tasks)
---
+++ test/test.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var expect = require('chai').expect,
+    webshot = require('webshot'),
+    resemble = require('node-resemble-js'),
+    fs = require('fs');
+
+describe('pointless test', function () {
+
+  it('does pointless things', function (done) {
+
+    // THIS CODE IS CRAP - IT WAS PURELY FOR INVESTIGATION PURPOSES AND IS COMPLETELY UNSUITABLE
+    // RETAINING FOR REFRENCE ONLY UNTIL I HAVE THE CHANCE TO WRITE A SUITABLE PROTOTYPE
+
+    webshot('google.com', 'google.png', function(err) {
+      // screenshot now saved to google.png
+    });
+
+    webshot('google.com', 'google-2.png', function(err) {
+      // screenshot now saved to google.png
+    });
+
+    var diff = resemble('google.png').compareTo('google-2.png').ignoreAntialiasing().onComplete(function (diffData) {
+      diffData.getDiffImage().pack().pipe(fs.createWriteStream('diff.png'));
+
+      // more info about writing the data as a png file here
+      // http://stackoverflow.com/questions/28746451/writing-buffer-response-from-resemble-js-to-file
+      // https://github.com/lksv/node-resemble.js/issues/4
+      // done();
+      /*
+      {
+        misMatchPercentage : 100, // %
+        isSameDimensions: true, // or false
+        dimensionDifference: { width: 0, height: -1 }, // defined if dimensions are not the same
+        getImageDataUrl: function(){}
+      }
+      */
+    });
+
+  });
+
+});
